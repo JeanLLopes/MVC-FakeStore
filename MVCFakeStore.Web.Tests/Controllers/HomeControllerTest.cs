9fdaa9fb6efdcf26c17dddcec1da2650bdea8001
JeanLLopes <jean.llopes@outlook.com>
2016-03-30 23:50:32 -0300

    Initial Commit

---
+++ MVCFakeStore.Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCFakeStore.Web;
+using MVCFakeStore.Web.Controllers;
+
+namespace MVCFakeStore.Web.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index_Is_A_ViewResult()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ MVCFakeStore.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFakeStore.Web.Controllers
+{
+    [RoutePrefix("Home")]
+    public class HomeController : Controller
+    {
+        [Route("~/")]
+        [Route]
+        [Route("Index")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
